c599817da83caf628dbf66ad8239918a74da9de4
Joanne Marie Batungbacal <100325689+JoanneMarieBatungbacal@users.noreply.github.com>
2022-05-01 23:25:57 +0800

    Add files via upload

---
+++ Project.py
@@ -0,0 +1,21 @@
+#initalize pygame
+pygame.init()
+ 
+#define our screen size
+SQUARESIZE = 100
+ 
+#define width and height of board
+width = COLUMN_COUNT * SQUARESIZE
+height = (ROW_COUNT+1) * SQUARESIZE
+ 
+size = (width, height)
+ 
+RADIUS = int(SQUARESIZE/2 - 5)
+ 
+screen = pygame.display.set_mode(size)
+#Calling function draw_board again
+draw_board(board)
+pygame.display.update()
+ 
+myfont = pygame.font.SysFont("monospace", 75)
+ 
